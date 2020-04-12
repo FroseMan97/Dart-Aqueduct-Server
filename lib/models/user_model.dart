d3cac29efc43ae75962b1ac3dfdda20261ffd1a5
s.lazarev <s.lazarev@cm.expert>
2020-04-12 18:22:01 +0300

    delete old files

--- lib/models/user_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:aqueduct/managed_auth.dart';
-import 'package:car_drivers/car_drivers.dart';
-
-class User extends ManagedObject<_User> implements _User, ManagedAuthResourceOwner<_User> {
-  @Serialize(input: true, output: false)
-  String password;
-}
-
-class _User extends ResourceOwnerTableDefinition {}
